31eb0ef087d89383d35c3cd6264450bdca5f385a
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-07 17:36:14 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@
 def is_same_class(obj, a_class):
     """return True if object is instance or False in otherwise"""
     def is_same_class(obj, a_class):
-        if isinstance(obj, a_class):
+        if type(obj) is a_class:
             return (True)
         return (False)
