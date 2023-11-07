dc01d64a4675b8f7a4a6e1c27639f1f2b11de808
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-07 17:07:16 +0100

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that check if the object is exactly
+ an instance of the specified class """
+
+
+def is_same_class(obj, a_class):
+    """return True if object is instance or False in otherwise"""
+    def is_same_class(obj, a_class):
+        if (isinstance(obj, a_class)):
+            return (True)
+        return (False)
