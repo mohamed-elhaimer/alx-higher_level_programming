7a14f313c755698a729b2758c478728955b40032
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-14 20:23:05 +0100

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+from base import Base
+"""class Rectangle inhherits from Base"""
+
+
+class Rectangle(Base):
+    """represent the rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize the rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter for width"""
+        return (self.__width)
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return (self.__height)
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter for x"""
+        return (self.__x)
+    @x.setter
+    def x(self, value):
+        """setter for x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter for y"""
+        return (self.__y)
+    @y.setter
+    def y(self, value):
+        """setter for y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
