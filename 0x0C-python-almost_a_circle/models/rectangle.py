5b4255e9c32a3393c80ee2edbcb6a965fe362ad3
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-17 11:18:06 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,71 +1,80 @@
 #!/usr/bin/python3
+"""Defines a rectangle class."""
 from models.base import Base
-"""class Rectangle inhherits from Base"""
 
 
 class Rectangle(Base):
-    """represent the rectangle"""
+    """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initialize the rectangle"""
-        super().__init__(id)
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        """getter for width"""
-        return (self.__width)
+        """Set/get the width of the Rectangle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """setter for width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """getter for height"""
-        return (self.__height)
+        """Set/get the height of the Rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """setter for height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """getter for x"""
-        return (self.__x)
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """setter for x"""
         if type(value) != int:
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """getter for y"""
-        return (self.__y)
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """setter for y"""
         if type(value) != int:
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
