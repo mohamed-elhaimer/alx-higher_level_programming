4dad4bbc988c1dafc6ae52626ccb092b1d1e3fe3
SimoLogiC <mohamedelheimer0@gmail.com>
2023-12-14 03:12:06 +0100

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,1 +1,1 @@
-SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` WHERE `month` = 7 AND `month` = 8 GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
