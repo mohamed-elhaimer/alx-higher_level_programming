75f6eb278536db27e768c3e687f21469233b8884
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-17 16:13:17 +0100

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.Connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states where name LEKE 'N%' ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states where name LIKE
+                'N%' ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
