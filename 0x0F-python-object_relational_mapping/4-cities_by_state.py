9cb9fdbbbe22491956b82ecbd8a2b6ebf29d618f
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-18 16:53:03 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute('''SELECT cities.id, cities.name, states.name FROM cities, states
-                 INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC''')
+    cur.execute('''SELECT cities.id, cities.name, states.name
+                FROM cities, states
+                 INNER JOIN states ON cities.state_id = states.id
+                 ORDER BY cities.id ASC''')
     rows = cur.fetchall()
     for row in rows:
         print(row)
