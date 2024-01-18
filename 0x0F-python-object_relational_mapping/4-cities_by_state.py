fd7170e913219679d8a7498e7dea288f4b6c4526
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-18 16:54:50 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute('''SELECT cities.id, cities.name, states.name
                 FROM cities, states
-                 INNER JOIN states ON cities.state_id = states.id
+                 WHERE cities.state_id = states.id
                  ORDER BY cities.id ASC''')
     rows = cur.fetchall()
     for row in rows:
