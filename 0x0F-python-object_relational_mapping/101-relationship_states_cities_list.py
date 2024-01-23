e16c4badca40efc7fefeeccd58cc1fc530008130
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-23 12:58:24 +0100

    101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+ lists all State objects, and corresponding City objects,
+ contained in the database hbtn_0e_101_usa
+"""
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).order_by(State.id)
+    for state in data:
+        print(str(state.id), state.name, sep=": ")
+        for city in state.cities:
+            print(" ", end="")
+            print(str(City.id), City.name, sep=": ")
