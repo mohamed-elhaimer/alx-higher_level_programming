41cc90dbdc23976478201fded41f8ef26adac427
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-30 18:18:11 +0100

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #request the server to respond with a message containing You got me!
-curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
