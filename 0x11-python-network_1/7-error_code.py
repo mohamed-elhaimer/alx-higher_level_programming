a48bc08e57e9d038c2e1c302fbb1fadf3ce3ec57
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 21:22:40 +0100

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ import requests
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
     if req.status_code >= 400:
-        print(req.status_code)
+        print("Error code: ".format(req.status_code))
     else:
         print(req.text)
