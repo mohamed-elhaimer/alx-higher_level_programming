3384d5b16bb8f4ca2e1a25baeb9123f39713193f
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 16:24:44 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         res = response.read()
-        print("Your email is: ".format(res.decode('utf-8')))
+        print(res.decode('utf-8'))
