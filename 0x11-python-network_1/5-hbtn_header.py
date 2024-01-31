0206d2d390c013ddb1238751eac360e2b56bad52
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 18:35:43 +0100

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays
+the value of the variable X-Request-Id
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    print(dict(req.text).get("X-Request-Id"))
