f2916145c210cba53d5ef40bb20dc7e0bb36a5fb
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 17:45:18 +0100

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ sends a request to the URL and displays the body of the response
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(e.code.decode('utf-8'))
