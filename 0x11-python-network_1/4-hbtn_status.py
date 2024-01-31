16b79c434ca1c714a2d0a05cd84808f876ecefc6
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 18:13:58 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("\tBody response:")
     print("\t- type: {}".format(type(r)))
-    print("\t - content: {}".format(r))
+    print("\t- content: {}".format(r))
