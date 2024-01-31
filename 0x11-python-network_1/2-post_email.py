8a094ae9d37ead2601f9ae097dcf643843d6c3a8
SimoLogic <mohamedelheimer0@gmail.com>
2024-01-31 16:17:42 +0100

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email as a parameter
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print("Your email is: ".format(response.decode('utf-8')))
