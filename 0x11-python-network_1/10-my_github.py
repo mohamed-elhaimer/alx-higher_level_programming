7510a66f9502145e80b75ebf9d3e699e8438e7df
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-01 17:02:35 +0100

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,8 +7,7 @@ from sys import argv
 import requests
 from requests.auth import HTTPBasicAuth
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
