2bd2dd9fd01ecc6a39f9835b57707ffccac1728f
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-05 13:26:40 +0100

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,9 @@
-#!/usr/bin/node
-const args = process.argv;
-function facto (a) {
-  if (isNaN(a)) {
-    return 1;
-  } else {
-    return a * facto(a - 1);
+#!/opt/homebrew/bin/node
+function factorial (n) {
+  let p = 1;
+  for (let i = 1; i <= n; i++) {
+    p *= i;
   }
+  return p;
 }
-const res = facto(args[2]);
-console.log(res);
+console.log(factorial((process.argv[2])));
