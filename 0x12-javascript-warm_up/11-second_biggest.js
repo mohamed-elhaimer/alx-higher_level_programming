7bdef1a6b80c6136d4ee19b24077427f1309eb57
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-05 16:37:31 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2)
+  const arr = process.argv.slice(2);
   const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
