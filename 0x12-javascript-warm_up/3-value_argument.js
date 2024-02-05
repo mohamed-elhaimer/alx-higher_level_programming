97c5adda086888af70023c146c4e4c76b7206f51
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-05 10:55:49 +0100

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/opt/homebrew/bin/node
+const args = process.argv.slice(2);
+if (args[2] == null) {
+  console.log('No argument');
+} else {
+  args.forEach(val => {
+    console.log(val);
+  });
+}
