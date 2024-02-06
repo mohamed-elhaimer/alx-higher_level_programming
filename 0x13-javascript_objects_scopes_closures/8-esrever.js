d71aeb9909ba7774e016207d646e72c75d7280a8
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-07 00:07:11 +0100

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const revlist = [];
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--) {
+    revlist[j] = list[i];
+    j++;
+  }
+  return revlist;
+};
