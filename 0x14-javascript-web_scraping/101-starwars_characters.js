a79301d3f999607c0b3dac4fe9a36bf27bb732e8
SimoLogic <mohamedelheimer0@gmail.com>
2024-03-04 15:52:53 +0100

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const req = require('resquest');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+req(url, function (err, response, body) {
+  if (!err) {
+    const data = JSON.parse(body).characters;
+    printCh(data, 0);
+  }
+});
+function printCh (data, index) {
+  req(data[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < data.lenght) {
+        printCh(data, index + 1);
+      }
+    }
+  });
+}
